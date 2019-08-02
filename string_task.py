2f2e41f3a918cca42cbc048a19d68a33c3cced6d
Mariam <Mariamhh98@hotmail.com>
2019-08-02 20:10:21 +0300

    string task

---
+++ string_task.py
@@ -0,0 +1,23 @@
+print("Mad libs where libs get Mad\n Start Below:\n")
+
+time = input("Number: ")
+items = input("Noun(plural): ")
+name = input("Name: ").title()
+scream = input("Any sentence: ").upper()
+action = input("Verb: ")
+
+print()
+
+print ("     It was", time,  "o'clock when I heard a knock at the door.\n",
+"    I opened the door and there was a box full of", items, "with a note saying \" From Mr.", name, "\".\n",
+"    Just as I closed the door I heard a scream \"", scream, "\".\n",
+"    I froze in place and all I could do was", action, ".")
+
+
+
+
+
+
+
+
+
